eddc7b03032baf088d6a0b0208c1327575fe9161
gludovatza <attila@gludovatz.hu>
2023-11-02 19:35:32 +0100

    8. rész: Jogosultsági rendszer összeépítése

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,24 +3,42 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Models\Device;
+use App\Models\Document;
+use App\Models\DeviceType;
+use App\Policies\RolePolicy;
+use App\Policies\UserPolicy;
+use App\Policies\DevicePolicy;
+use App\Policies\DocumentPolicy;
+use App\Policies\DeviceTypePolicy;
+use App\Policies\PermissionPolicy;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
+  /**
+   * The model to policy mappings for the application.
+   *
+   * @var array<class-string, class-string>
+   */
+  protected $policies = [
+    Permission::class => PermissionPolicy::class,
+    Role::class => RolePolicy::class,
+    User::class => UserPolicy::class,
 
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+    DeviceType::class => DeviceTypePolicy::class,
+    Device::class => DevicePolicy::class,
+    Document::class => DocumentPolicy::class,
+  ];
+
+  /**
+   * Register any authentication / authorization services.
+   */
+  public function boot(): void
+  {
+    //
+  }
 }
